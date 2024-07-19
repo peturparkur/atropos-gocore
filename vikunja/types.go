ac275b584e3d2bd2ab39ff7d8f678f818cc4b673
atropos <sv7n@pm.me>
2024-07-19 21:18:10 +0100

    small omissions.

--- vikunja/types.go
+++ vikunja/types.go
@@ -12,32 +12,32 @@ type Owner struct {
 
 // VikunjaProject is a struct that represents a project in Vikunja
 type Project struct {
-	ID                    int          `json:"id"`
-	Title                 string       `json:"title"`
-	HexColor              string       `json:"hex_color"`
-	BackgroundBlurHash    string       `json:"background_blur_hash"`
-	Position              float64      `json:"position"`
-	Created               string       `json:"created"`
-	Updated               string       `json:"updated"`
-	Description           string       `json:"description"`
-	DefaultBucketID       int          `json:"default_bucket_id"`
-	DoneBucketID          int          `json:"done_bucket_id"`
-	Identifier            string       `json:"identifier"`
-	IsArchived            bool         `json:"is_archived"`
-	IsFavorite            bool         `json:"is_favorite"`
-	Owner                 VikunjaOwner `json:"owner"`
-	ParentProjectID       int          `json:"parent_project_id"`
-	BackgroundInformation interface{}  `json:"background_information"`
+	ID                    int         `json:"id"`
+	Title                 string      `json:"title"`
+	HexColor              string      `json:"hex_color"`
+	BackgroundBlurHash    string      `json:"background_blur_hash"`
+	Position              float64     `json:"position"`
+	Created               string      `json:"created"`
+	Updated               string      `json:"updated"`
+	Description           string      `json:"description"`
+	DefaultBucketID       int         `json:"default_bucket_id"`
+	DoneBucketID          int         `json:"done_bucket_id"`
+	Identifier            string      `json:"identifier"`
+	IsArchived            bool        `json:"is_archived"`
+	IsFavorite            bool        `json:"is_favorite"`
+	Owner                 Owner       `json:"owner"`
+	ParentProjectID       int         `json:"parent_project_id"`
+	BackgroundInformation interface{} `json:"background_information"`
 }
 
 // VikunjaWebhook represents a webhook in Vikunja
 type Webhook struct {
-	ID        int           `json:"id"`
-	TargetURL string        `json:"target_url"`
-	Events    []string      `json:"events"`
-	ProjectID int           `json:"project_id"`
-	Secret    *string       `json:"secret,omitempty"`
-	Created   *string       `json:"created,omitempty"`
-	Updated   *string       `json:"updated,omitempty"`
-	CreatedBy *VikunjaOwner `json:"created_by,omitempty"`
+	ID        int      `json:"id"`
+	TargetURL string   `json:"target_url"`
+	Events    []string `json:"events"`
+	ProjectID int      `json:"project_id"`
+	Secret    *string  `json:"secret,omitempty"`
+	Created   *string  `json:"created,omitempty"`
+	Updated   *string  `json:"updated,omitempty"`
+	CreatedBy *Owner   `json:"created_by,omitempty"`
 }
