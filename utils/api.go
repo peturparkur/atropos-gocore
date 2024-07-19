2dac09ed2a1347e0757bedaba1d9242f63c1112e
atropos <sv7n@pm.me>
2024-07-19 22:15:12 +0100

    added put and post

--- utils/api.go
+++ utils/api.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -9,7 +10,7 @@ import (
 )
 
 // MakeGetRequest makes a GET request to the specified endpoint. If token is not "" it will be added to the request as a Bearer token.
-func MakeGetRequest[responseType any](l *zap.SugaredLogger, apiBaseURL, endpoint, token string, res *responseType) error {
+func MakeGetRequest[responseType any](l *zap.SugaredLogger, apiBaseURL, endpoint, token string, response *responseType) error {
 	req, err := http.NewRequest("GET", apiBaseURL+endpoint, nil)
 	if err != nil {
 		return err
@@ -31,13 +32,89 @@ func MakeGetRequest[responseType any](l *zap.SugaredLogger, apiBaseURL, endpoint
 		return err
 	}
 
-	if err := json.Unmarshal(body, &res); err != nil {
+	if err := json.Unmarshal(body, &response); err != nil {
 		l.Errorw("Failed to unmarshal response", "error", err, "body", string(body))
 		return err
 	}
 
 	return nil
 }
+
+// MakePostRequest makes a POST request to the specified endpoint. If token is not "" it will be added to the request as a Bearer token.
+func MakePostRequest[requestType, responseType any](l *zap.SugaredLogger, apiBaseURL, endpoint, token string, request requestType, response *responseType) error {
+	jsonData, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", apiBaseURL+endpoint, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	if token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(body, &response); err != nil {
+		l.Errorw("Failed to unmarshal response", "error", err, "body", string(body))
+		return err
+	}
+
+	return nil
+}
+
+// MakePutRequest makes a POST request to the specified endpoint. If token is not "" it will be added to the request as a Bearer token.
+func MakePutRequest[requestType, responseType any](l *zap.SugaredLogger, apiBaseURL, endpoint, token string, request requestType, response *responseType) error {
+	jsonData, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("PUT", apiBaseURL+endpoint, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	if token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(body, &response); err != nil {
+		l.Errorw("Failed to unmarshal response", "error", err, "body", string(body))
+		return err
+	}
+
+	return nil
+}
+
+///
 
 //
 // func makeAPIRequest[respType any](_ *zap.SugaredLogger, token, apiURL, endpoint, kind string, res *respType, msg interface{}) error {
