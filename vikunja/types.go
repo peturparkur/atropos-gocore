411373c98dc89a6d80fefde3dd09401f55d303c4
atropos <sv7n@pm.me>
2024-07-20 18:01:34 +0100

    Adedd new types for vikunja

--- vikunja/types.go
+++ vikunja/types.go
@@ -10,7 +10,9 @@ type Owner struct {
 	Updated  string `json:"updated"`
 }
 
-// VikunjaProject is a struct that represents a project in Vikunja
+type Doer Owner
+
+// Project is a struct that represents a project in Vikunja
 type Project struct {
 	ID                    int         `json:"id"`
 	Title                 string      `json:"title"`
@@ -30,7 +32,40 @@ type Project struct {
 	BackgroundInformation interface{} `json:"background_information"`
 }
 
-// VikunjaWebhook represents a webhook in Vikunja
+// Task represents a task in Vikunja
+type Task struct {
+	ID                     int         `json:"id"`
+	Title                  string      `json:"title"`
+	Description            string      `json:"description"`
+	Done                   bool        `json:"done"`
+	DoneAt                 string      `json:"done_at"`
+	DueDate                string      `json:"due_date"`
+	Reminders              interface{} `json:"reminders"`
+	ProjectID              int         `json:"project_id"`
+	RepeatAfter            int         `json:"repeat_after"`
+	RepeatMode             int         `json:"repeat_mode"`
+	Priority               int         `json:"priority"`
+	StartDate              string      `json:"start_date"`
+	EndDate                string      `json:"end_date"`
+	Assignees              interface{} `json:"assignees"`
+	Labels                 interface{} `json:"labels"`
+	HexColor               string      `json:"hex_color"`
+	PercentDone            int         `json:"percent_done"`
+	Identifier             string      `json:"identifier"`
+	Index                  int         `json:"index"`
+	RelatedTasks           interface{} `json:"related_tasks"`
+	Attachments            interface{} `json:"attachments"`
+	CoverImageAttachmentID int         `json:"cover_image_attachment_id"`
+	IsFavorite             bool        `json:"is_favorite"`
+	Created                string      `json:"created"`
+	Updated                string      `json:"updated"`
+	BucketID               int         `json:"bucket_id"`
+	Position               float64     `json:"position"`
+	KanbanPosition         float64     `json:"kanban_position"`
+	CreatedBy              Owner       `json:"created_by"`
+}
+
+// Webhook represents a webhook in Vikunja
 type Webhook struct {
 	ID        int      `json:"id"`
 	TargetURL string   `json:"target_url"`
@@ -40,4 +75,17 @@ type Webhook struct {
 	Created   *string  `json:"created,omitempty"`
 	Updated   *string  `json:"updated,omitempty"`
 	CreatedBy *Owner   `json:"created_by,omitempty"`
+}
+
+// WebhookCallbackData represents the data that is sent to a webhook callback
+type WebhookCallbackData struct {
+	Doer Doer `json:"doer"`
+	Task Task `json:"task"`
+}
+
+// WebhookCallback represents a webhook callback
+type WebhookCallback struct {
+	EventName string              `json:"event_name"`
+	Time      string              `json:"time"`
+	Data      WebhookCallbackData `json:"data"`
 }
